0dfb672c00458cd39690a231dfdd9025a38efa11
Franco Parodi <franco.parodi@aol.com>
2020-11-08 12:10:55 +0100

    controller implementations

--- flaskr/rover.py
+++ flaskr/rover.py
@@ -1,10 +1,24 @@
 class Rover():
 
-    # speed value from 0 to 100%
-    speed = 0
-    # status: 'stop', 'forward', 'backward', 'clockwise', 'counter-clockwise' 
-    status = 'stop'
+    import sys
+    try:
+        import RPi.GPIO as GPIO
+    except (RuntimeError, ModuleNotFoundError):
+        import fake_rpi
+        GPIO = fake_rpi.RPi.GPIO
+        sys.modules['RPi'] = fake_rpi.RPi
+        sys.modules['RPi.GPIO'] = fake_rpi.RPi.GPIO
+        sys.modules['smbus'] = fake_rpi.smbus
 
     def __init__(self):
         self.speed = 0
-        self.move_mode = 'stop'
+        self.status = 'stop' # values: 'stop', 'forward', 'backward', 'clockwise', 'counter-clockwise'
+        self.leftFrontMotor = 23 # GPIO23, pin16
+        self.leftBackMotor = 24 # GPIO24, pin18
+        self.rightFrontMotor = 17 # GPIO17, pin11
+        self.rightBackMotor = 27 # GPIO27, pin13
+        self.leftSideMotorsEnabling = 18 # GPIO18, pin12
+        self.rightSideMotorsEnabling = 22 # GPIO22, pin15
+        self.frequency = 50
+        self.leftMotors = None
+        self.rightMotors = None
---
+++ flaskr/rover_controller.py
@@ -0,0 +1,60 @@
+import time, atexit, rover
+
+def gpioSetup():
+    rover.GPIO.setmode(rover.GPIO.BCM)
+
+    # Motors
+    rover.GPIO.setup(rover.leftFrontMotor, rover.GPIO.OUT)
+    rover.GPIO.setup(rover.leftBackMotor, rover.GPIO.OUT)
+    rover.GPIO.setup(rover.rightFrontMotor, rover.GPIO.OUT)
+    rover.GPIO.setup(rover.rightBackMotor, rover.GPIO.OUT)
+    # Motors Directions
+    setLeftMotorsDirection('forward')
+    setRightMotorsDirection('forward')
+    time.sleep(1)
+
+    # Enable left side motors
+    rover.GPIO.setup(rover.leftSideMotorsEnabling, rover.GPIO.OUT)
+    rover.leftMotors = rover.GPIO.PWM(rover.leftSideMotorsEnabling, rover.frequency)
+    # Enable right side motors
+    rover.GPIO.setup(rover.rightSideMotorsEnabling, rover.GPIO.OUT)
+    rover.rightMotors = rover.GPIO.PWM(rover.rightSideMotorsEnabling, rover.frequency)
+
+def setLeftMotorsDirection(direction):
+    if (direction == 'forward'):
+        rover.GPIO.output(rover.leftFrontMotor, rover.GPIO.HIGH)
+        rover.GPIO.output(rover.leftBackMotor, rover.GPIO.LOW)
+    else:
+        rover.GPIO.output(rover.leftFrontMotor, rover.GPIO.LOW)
+        rover.GPIO.output(rover.leftBackMotor, rover.GPIO.HIGH)
+
+def setRightMotorsDirection(direction):
+    if (direction == 'forward'):
+        rover.GPIO.output(rover.rightFrontMotor, rover.GPIO.HIGH)
+        rover.GPIO.output(rover.rightBackMotor, rover.GPIO.LOW)
+    else:
+        rover.GPIO.output(rover.rightFrontMotor, rover.GPIO.LOW)
+        rover.GPIO.output(rover.rightBackMotor, rover.GPIO.HIGH)
+
+def stopMotors():
+    rover.leftMotors.stop()
+    rover.rightMotors.stop()
+
+def startMotors(speed):
+    rover.leftMotors.start(speed)
+    rover.rightMotors.start(speed)
+
+def setSpeed(speed):
+    rover.leftMotors.ChangeDutyCycle(speed)
+    rover.rightMotors.ChangeDutyCycle(speed)
+
+def demo():
+    # GPIO Setup
+    gpioSetup()
+    
+# Safa terminating
+def cleanUp():  
+    stopMotors()
+    rover.GPIO.cleanup()
+
+atexit.register(cleanUp)
