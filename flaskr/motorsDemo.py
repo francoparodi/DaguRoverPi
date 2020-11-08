038af744c543fb2b29055ad565fd8df005c98c65
Franco Parodi <franco.parodi@aol.com>
2020-11-08 11:23:54 +0100

    Add motorsDemo

---
+++ flaskr/motorsDemo.py
@@ -0,0 +1,100 @@
+import RPi.GPIO as GPIO
+import time
+
+leftFrontMotor = 23 # GPIO23, pin16
+leftBackMotor = 24 # GPIO24, pin18
+rightFrontMotor = 17 # GPIO17, pin11
+rightBackMotor = 27 # GPIO27, pin13
+leftSideMotorsEnabling = 18 # GPIO18, pin12
+rightSideMotorsEnabling = 22 # GPIO22, pin15
+frequency = 50
+leftMotors = None
+rightMotors = None
+initialSpeed = 10
+
+def gpioSetup():
+    GPIO.setmode(GPIO.BCM)
+
+    # Motors
+    GPIO.setup(leftFrontMotor, GPIO.OUT)
+    GPIO.setup(leftBackMotor, GPIO.OUT)
+    GPIO.setup(rightFrontMotor, GPIO.OUT)
+    GPIO.setup(rightBackMotor, GPIO.OUT)
+    # Motors Directions
+    setLeftMotorsDirection('forward')
+    setRightMotorsDirection('forward')
+    time.sleep(1)
+
+    # Enable left side motors
+    GPIO.setup(leftSideMotorsEnabling, GPIO.OUT)
+    global leftMotors
+    leftMotors = GPIO.PWM(leftSideMotorsEnabling, frequency)
+    # Enable right side motors
+    GPIO.setup(rightSideMotorsEnabling, GPIO.OUT)
+    global rightMotors
+    rightMotors = GPIO.PWM(rightSideMotorsEnabling, frequency)
+
+def setLeftMotorsDirection(direction):
+    if (direction == 'forward'):
+        GPIO.output(leftFrontMotor, GPIO.HIGH)
+        GPIO.output(leftBackMotor, GPIO.LOW)
+    else:
+        GPIO.output(leftFrontMotor, GPIO.LOW)
+        GPIO.output(leftBackMotor, GPIO.HIGH)
+
+def setRightMotorsDirection(direction):
+    if (direction == 'forward'):
+        GPIO.output(rightFrontMotor, GPIO.HIGH)
+        GPIO.output(rightBackMotor, GPIO.LOW)
+    else:
+        GPIO.output(rightFrontMotor, GPIO.LOW)
+        GPIO.output(rightBackMotor, GPIO.HIGH)
+
+def stopMotors():
+    leftMotors.stop()
+    rightMotors.stop()
+
+def startMotors(speed = initialSpeed):
+    leftMotors.start(speed)
+    rightMotors.start(speed)
+
+def setSpeed(speed):
+    leftMotors.ChangeDutyCycle(speed)
+    rightMotors.ChangeDutyCycle(speed)
+
+def demo():
+    # GPIO Setup
+    gpioSetup()
+    time.sleep(1)
+
+    # Start motors (power of 10%)
+    startMotors(initialSpeed)
+    time.sleep(2)
+
+    # Increase speed
+    setSpeed(30)
+    time.sleep(2)
+
+    # Stop motors
+    stopMotors()
+    time.sleep(1)
+
+    # Change direction
+    setLeftMotorsDirection('backward')
+    setRightMotorsDirection('backward')
+
+    # Start motors (power of 10%)
+    startMotors(initialSpeed)
+    time.sleep(3)
+
+    # increase speed
+    setSpeed(30)
+    time.sleep(3)
+
+    # stop motors
+    stopMotors()
+    time.sleep(1)
+
+    GPIO.cleanup()
+
+demo()
