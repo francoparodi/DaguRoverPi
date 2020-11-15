c886956412827b2c705efc9ec14e8524202ce755
Franco Parodi <franco.parodi@aol.com>
2020-11-15 10:48:00 +0100

    Fix pin number

--- flaskr/rover.py
+++ flaskr/rover.py
@@ -12,12 +12,13 @@ class Rover():
 
     power = 0
     status = 'STOP' # values: 'STOP', 'FORWARD', 'BACKWARD', 'CLOCKWISE', 'COUNTER-CLOCKWISE'
-    leftFrontMotor = 23 # GPIO23, pin16 Blue
-    leftBackMotor = 24 # GPIO24, pin18 Green
-    rightFrontMotor = 17 # GPIO17, pin11 Brown
-    rightBackMotor = 27 # GPIO27, pin13 Violet
-    leftSideMotorsEnabling = 18 # GPIO18, pin12 PWM Yellow
-    rightSideMotorsEnabling = 12 # GPIO12, pin32 PWM White
+    rightBackMotor = 5 # GPIO5, pin29 Blue (Channel01)
+    rightFrontMotor = 6 # GPIO06, pin31 Green (Channel03)
+    leftBackMotor = 16 # GPIO16, pin36 Violet (Channel02)
+    leftFrontMotor = 17 # GPIO17, pin11 Brown (Channel04)
+    rightSideMotorsEnabling = 12 # GPIO12, pin32 PWM White (Channel 01/03)
+    leftSideMotorsEnabling = 18 # GPIO18, pin12 PWM Yellow (Channel 02/04)
+
     frequency = 50
     leftMotors = None
     rightMotors = None
